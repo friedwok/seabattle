f4d1e37d03275bfe1010ead912e4ef058c7a2c7c
Pavel Petrov <slow19@mail.ru>
2019-04-06 01:42:44 +0300

    client folder commit

---
+++ client/client.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<sys/types.h>
+#include<cstdlib>
+#include<ctype.h>
+#include "pregame.h"
+#include<unistd.h>
+
+int main(int argc, char **argv)
+{
+	int ls, port, c, i = 0, player_number, fl = 0;
+	struct sockaddr_in addr;
+	char *serv_ip, *tmp;
+
+	if(argc < 2) {
+		fprintf(stderr, "Program has one argument - number of port\n");
+		return 1;
+	}
+
+	try {
+		int s = sscanf(argv[1], "%d", &port);
+		if(!s)
+			throw "Wrong parameter";
+	} catch(const char *exception) {
+		fprintf(stderr, "%s\n", exception);
+		return 1;
+	}
+
+	printf("Please, input IP address of the server\n");
+	c = getchar();
+	while(!isspace(c)) {
+		if(i == 0) {
+			tmp = (char*)malloc(1);
+		} else {
+			tmp = (char*)realloc(tmp, i+1);
+		}
+		tmp[i] = c;
+		i++;
+		c = getchar();
+		if(i > 16) {
+			printf("false IP\n");
+			exit(1);
+		}
+	}
+	tmp = (char*)realloc(tmp,i+1);
+	tmp[i] = 0;
+
+	serv_ip = tmp;
+
+	ls = socket(AF_INET, SOCK_STREAM, 0);
+
+	port = 8080;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	if(!inet_aton(serv_ip, &(addr.sin_addr))) {
+		printf("IP adress is not valid\n");
+	}
+
+	if(0 != connect(ls, (struct sockaddr*)&addr, sizeof(addr))) {
+		perror("connect");
+		exit(1);
+	}
+
+	printf("Connection established\n");
+	pre_game(ls, &player_number, &fl);
+	//start_the_game(ls, &fl);
+	return 0;
+}
---
+++ client/pregame.h
@@ -0,0 +1,8 @@
+#ifndef PREGAME_H_SENTRY
+#define PREGAME_H_SENTRY
+
+void check_buf(int *buf, int *fl, int player_num, int& pl_num_flag);
+
+void pre_game(int ls, int *pl_num, int *fl);
+
+#endif
